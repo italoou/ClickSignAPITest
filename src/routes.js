7e863ae4cc63cf653e4e7143859660bda8aa922c
italo lima <italo.itlima@gmail.com>
2022-09-01 01:09:01 -0300

    iniciando repositorio

---
+++ src/routes.js
@@ -0,0 +1,56 @@
+const router = require('express').Router();
+const axios = require('axios');
+const fs = require('fs');
+
+router.get('/', async (req, res) => {
+  let text;
+  await axios.get(`https://sandbox.clicksign.com/api/v1/accounts?access_token=${process.env.APIKEY}`)
+  .then( (res) => {
+    text = res.data;
+  })
+  .catch((error) => { console.log(error)});
+  console.log(text);
+  
+  return res.status(200).send({nameApp: "ClickSignApiTest",
+                               APITest: text});
+})
+
+router.get('/modelos', async (req, res) => {
+  let text;
+  await axios.get(`https://sandbox.clicksign.com/api/v2/templates?access_token=${process.env.APIKEY}`)
+  .then((res) => {
+    text = res.data;
+  })
+  .catch((error) => {console.log(error)});
+
+  return res.status(200).send(text);
+})
+
+router.post('/modelos', async (req, res) => {
+  let retorno;
+  let status;
+  let id = 0;
+  await axios.post(`https://sandbox.clicksign.com/api/v1/templates/889bbee7-b2d7-4270-b0b4-31b1f7c4cf5c/documents?access_token=${process.env.APIKEY}`, req.body)
+  .then((res) => {
+    retorno = res.data;
+    status = res.status;
+    id = res.data.document.key;
+  })
+  .catch((error) => {
+    return res.status(error.response.status).send(error.response.data)
+  });
+
+  const obj = {id, texto: req.body.document.template.data.Texto, assinado: false };
+  const documento = await fs.writeFileSync('src/database.json', JSON.stringify(obj));
+
+  return res.status(status).send(retorno);
+})
+
+router.post('/assinar', async (req, res) => {
+  const documento = await fs.readFileSync('src/database.json', 'utf8');
+  let doc = JSON.parse(documento);
+  documento.assinado = true;
+  await fs.writeFileSync('src/database.json', JSON.stringify(documento));
+})
+
+module.exports = router
---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = require('./routes');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(router);
+
+const PORT = 8080;
+app.listen(PORT, () => console.log(`ClickSignApiTest Server is running on http://localhost:${PORT}`));
