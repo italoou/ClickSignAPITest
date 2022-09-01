6b171cc93ee6def9c3f9911a012a977a4bec248a
italo lima <italo.itlima@gmail.com>
2022-09-01 01:45:49 -0300

    adicionando port ao .env

--- src/server.js
+++ src/server.js
@@ -10,5 +10,5 @@ app.use(express.json());
 
 app.use(router);
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`ClickSignApiTest Server is running on http://localhost:${PORT}`));
